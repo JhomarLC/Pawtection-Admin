28e345fdb0abf8422d483c2ea96c0833194f662f
jhomarcandelario <89998631+DonMucho@users.noreply.github.com>
2024-11-26 00:48:34 +0800

    Added root in vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    chunkSizeWarningLimit: 3000,
-  },
-})
+	root: "./", // Path where index.html is located
+	plugins: [react()],
+	base: "/",
+	build: {
+		chunkSizeWarningLimit: 3000,
+	},
+});
